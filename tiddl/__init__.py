3a8424c4d8eb3ccb0976b7c37f3ccf7d1dffc529
oskvr37 <oskarxpd@gmail.com>
2024-07-20 23:14:50 +0200

    ♻️ use auth

--- tiddl/__init__.py
+++ tiddl/__init__.py
@@ -1,16 +1,16 @@
 import argparse
 from .api import TidalApi
+from .auth import getDeviceAuth, getToken
 
 
 def main():
     parser = argparse.ArgumentParser(description="TIDDL, the Tidal Downloader")
     print("✅ TIDDL installed!")
 
-    api = TidalApi()
-    auth = api.getDeviceAuth()
+    auth = getDeviceAuth()
     print(f"Go to https://{auth['verificationUriComplete']} and add device!")
     input("Hit enter when you are ready")
-    token = api.getToken(auth["deviceCode"])
+    token = getToken(auth["deviceCode"])
     print(token)
 
 
