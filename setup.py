2c5d3f28474f703451ff8d66e420285f7bd1c539
oskvr37 <oskarxpd@gmail.com>
2024-12-24 14:38:53 +0100

    🚀 bump to `2.0.0` and update setup

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="tiddl",
-    version="1.8.3",
-    description="TIDDL (Tidal Downloader) is a Python CLI application that allows downloading Tidal tracks.",
+    version="2.0.0",
+    description="TIDDL (Tidal Downloader) is CLI application that allows downloading Tidal tracks.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     readme="README.md",
     author="oskvr37",
     packages=find_packages(),
     entry_points={
-        "console_scripts": ["tiddl=tiddl:main", "tiddl2=tiddl.cli:cli"],
+        "console_scripts": ["tiddl=tiddl.cli:cli"],
     },
 )
