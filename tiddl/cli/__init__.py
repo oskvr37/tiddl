bf70635c948b49c8dcf49922f4f77d625388dffa
oskvr37 <oskarxpd@gmail.com>
2025-01-03 15:39:32 +0100

    ✨ add logging

--- tiddl/cli/__init__.py
+++ tiddl/cli/__init__.py
@@ -1,16 +1,25 @@
 import click
+import logging
 
 from .ctx import ContextObj, passContext, Context
 from .auth import AuthGroup
 from .download import UrlGroup, FavGroup, SearchGroup, FileGroup
 from .config import ConfigCommand
 
+
 @click.group()
 @passContext
-@click.option("--verbose", is_flag=True, help="Show debug logs")
+@click.option("--verbose", "-v", is_flag=True, help="Show debug logs")
 def cli(ctx: Context, verbose: bool):
     """TIDDL - Download Tidal tracks ✨"""
-    ctx.obj = ContextObj(verbose)
+    ctx.obj = ContextObj()
+
+    logging.basicConfig(
+        level=logging.DEBUG if verbose else logging.INFO,
+        handlers=[logging.StreamHandler()],
+        format="%(levelname)s [%(name)s.%(funcName)s] %(message)s",
+    )
+
 
 cli.add_command(ConfigCommand)
 cli.add_command(AuthGroup)
