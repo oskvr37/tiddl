59e53b2e1418b4303b7246b70fafc31468967ed0
oskvr37 <oskarxpd@gmail.com>
2025-01-18 13:19:00 +0100

    ✨ add `TrackCollector`

--- tiddl/cli/download/__init__.py
+++ tiddl/cli/download/__init__.py
@@ -9,6 +9,47 @@ from ..ctx import Context, passContext
 
 from tiddl.download import downloadTrackStream
 from tiddl.models import TrackArg, ARG_TO_QUALITY, Track
+from tiddl.utils import TidalResource
+from tiddl.api import TidalApi
+
+
+class TrackCollector:
+    def __init__(self, api: TidalApi):
+        self.api = api
+        self.tracks: list[Track] = []
+
+    def addResource(self, resource: TidalResource):
+        try:
+            match resource.resource_type:
+                case "track":
+                    track = self.api.getTrack(resource.resource_id)
+                    self._addTrack(track)
+
+                case "album":
+                    album_tracks = self.api.getAlbumItems(resource.resource_id)
+                    self._addItems(album_tracks.items)
+
+                case "playlist":
+                    playlist_tracks = self.api.getPlaylistItems(resource.resource_id)
+                    self._addItems(playlist_tracks.items)
+
+                case "artist":
+                    artist_albums = self.api.getArtistAlbums(resource.resource_id)
+                    for artist_album in artist_albums.items:
+                        album_tracks = self.api.getAlbumItems(artist_album.id)
+                        self._addItems(album_tracks.items)
+
+        except Exception as e:
+            print(f"Error in adding resource: {resource}, {e}")
+
+    def _addTrack(self, track: Track):
+        if track.allowStreaming:
+            self.tracks.append(track)
+
+    def _addItems(self, items):
+        for item in items:
+            if item.type == "track":
+                self._addTrack(item.item)
 
 
 @click.command("download")
@@ -17,37 +58,21 @@ from tiddl.models import TrackArg, ARG_TO_QUALITY, Track
 def DownloadCommand(ctx: Context, quality: TrackArg):
     """Download the tracks"""
 
+    api = ctx.obj.getApi()
+    track_collector = TrackCollector(api)
+
+    for resource in ctx.obj.resources:
+        track_collector.addResource(resource)
+
+    if not track_collector.tracks:
+        click.echo("No tracks found.")
+        return
+
     download_quality = ARG_TO_QUALITY[
         quality or ctx.obj.config.config["download"]["quality"]
     ]
 
-    api = ctx.obj.getApi()
-    tracks: list[Track] = []
-
-    def addTrack(track: Track):
-        if track.allowStreaming:
-            tracks.append(track)
-
-    for resource in ctx.obj.resources:
-        match resource.resource_type:
-            case "track":
-                try:
-                    track = api.getTrack(resource.resource_id)
-                    addTrack(track)
-                except Exception as e:
-                    print(e)
-
-            case "album":
-                album_tracks = api.getAlbumItems(resource.resource_id)
-                for album_item in album_tracks.items:
-                    if album_item.type == "track":
-                        addTrack(album_item.item)
-
-    if not tracks:
-        click.echo("No tracks found.")
-        return
-
-    for track in tracks:
+    for track in track_collector.tracks:
         click.echo(f"Downloading {track.title}")
         track_stream = api.getTrackStream(track.id, download_quality)
         stream_data, file_extension = downloadTrackStream(track_stream)
