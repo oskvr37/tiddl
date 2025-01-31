204596932013672c8d5f6b6a16182abaece30dea
oskvr37 <oskarxpd@gmail.com>
2025-01-31 20:10:01 +0100

    refactor to less indents

--- examples/concurrent_download_rich.py
+++ examples/concurrent_download_rich.py
@@ -21,78 +21,82 @@ from tiddl.models.api import PlaylistItems
 from tiddl.models.resource import Track
 
 
-if __name__ == "__main__":
-    WORKERS_COUNT = 4
-    PLAYLIST_UUID = "84974059-76af-406a-aede-ece2b78fa372"
-    ALBUM_ID = 103805723
+WORKERS_COUNT = 4
+PLAYLIST_UUID = "84974059-76af-406a-aede-ece2b78fa372"
+ALBUM_ID = 103805723
 
-    console = Console()
-    logging.basicConfig(
-        level=logging.DEBUG, handlers=[RichHandler(console=console)]
+console = Console()
+logging.basicConfig(
+    level=logging.DEBUG, handlers=[RichHandler(console=console)]
+)
+
+logging.getLogger("urllib3").setLevel(logging.ERROR)
+
+config = Config.fromFile()  # load config from default directory
+
+api = TidalApi(config.auth.token, config.auth.user_id, config.auth.country_code)
+
+progress = Progress(
+    TextColumn("{task.description}"),
+    BarColumn(bar_width=40),
+    console=console,
+    transient=True,
+    auto_refresh=True,
+)
+
+
+def handleTrackDownload(task_id: TaskID, track: Track):
+    total = randint(10, 30)
+    progress.update(task_id, total=total, visible=True)
+    progress.start_task(task_id)
+
+    # simulate track download
+
+    for _ in range(total):
+        sleep(0.1)
+        progress.update(task_id, advance=1)
+
+    console.log(track.title)
+
+    progress.remove_task(task_id)
+
+
+progress.start()
+
+pool = ThreadPoolExecutor(max_workers=WORKERS_COUNT)
+
+
+def submitTrack(track: Track):
+    task_id = progress.add_task(
+        description=track.title,
+        track=track,
+        start=False,
+        visible=False,
     )
 
-    logging.getLogger("urllib3").setLevel(logging.ERROR)
+    pool.submit(handleTrackDownload, task_id=task_id, track=track)
 
-    config = Config.fromFile()  # load config from default directory
 
-    api = TidalApi(
-        config.auth.token, config.auth.user_id, config.auth.country_code
-    )
+# NOTE: these api requests will run one by one,
+# we will need to add some sleep between requests
 
-    with Progress(
-        TextColumn("{task.description}"),
-        BarColumn(bar_width=40),
-        console=console,
-        transient=True,
-        auto_refresh=True
-    ) as progress:
+playlist_items = api.getPlaylistItems(playlist_uuid=PLAYLIST_UUID, limit=25)
 
-        def handleTrackDownload(task_id: TaskID, track: Track):
-            total = randint(10, 30)
-            progress.update(task_id, total=total, visible=True)
-            progress.start_task(task_id)
+for item in playlist_items.items:
+    track = item.item
 
-            # simulate track download
+    if isinstance(track, PlaylistItems.PlaylistTrackItem.PlaylistTrack):
+        submitTrack(track)
 
-            for _ in range(total):
-                sleep(0.1)
-                progress.update(task_id, advance=1)
+album_items = api.getAlbumItems(album_id=ALBUM_ID, limit=14)
 
-            console.log(track.title)
+for item in album_items.items:
+    track = item.item
 
-            progress.remove_task(task_id)
+    if isinstance(track, Track):
+        submitTrack(track)
 
-        with ThreadPoolExecutor(max_workers=WORKERS_COUNT) as pool:
+# cleanup
 
-            def submitTrack(track: Track):
-                task_id = progress.add_task(
-                    description=track.title,
-                    track=track,
-                    start=False,
-                    visible=False,
-                )
-
-                pool.submit(handleTrackDownload, task_id=task_id, track=track)
-
-            playlist_items = api.getPlaylistItems(
-                playlist_uuid=PLAYLIST_UUID, limit=25
-            )
-
-            for item in playlist_items.items:
-                track = item.item
-
-                if isinstance(
-                    track, PlaylistItems.PlaylistTrackItem.PlaylistTrack
-                ):
-                    submitTrack(track)
-
-            album_items = api.getAlbumItems(album_id=ALBUM_ID, limit=14)
-
-            for item in album_items.items:
-                track = item.item
-
-                if isinstance(track, Track):
-                    submitTrack(track)
-
-            # NOTE: these api requests will run one by one,
-            # we will need to add some sleep between requests
+pool.shutdown(wait=True)
+progress.stop()
