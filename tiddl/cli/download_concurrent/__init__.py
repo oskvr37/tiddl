4080e49b288e55b6cc9901abf538530ea9999962
oskvr37 <oskarxpd@gmail.com>
2025-02-08 19:25:34 +0100

    download track and video

--- tiddl/cli/download_concurrent/__init__.py
+++ tiddl/cli/download_concurrent/__init__.py
@@ -1,32 +1,30 @@
 import logging
-import click
-
-from ..ctx import Context, passContext
-
-from typing import List, Literal
-
 from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+from typing import List, Literal, Union
 
-from rich.console import Console
-from rich.logging import RichHandler
+import click
+from requests import Session
 from rich.progress import (
     BarColumn,
     Progress,
     TextColumn,
 )
 
-from tiddl.download import downloadTrackStream
+from tiddl.download import parseTrackStream, parseVideoStream
+from tiddl.exceptions import ApiError, AuthError
+from tiddl.metadata import Cover, addMetadata
+from tiddl.models.api import AlbumItemsCredits, PlaylistItems
+from tiddl.models.constants import ARG_TO_QUALITY, TrackArg
+from tiddl.models.resource import Track, Video
 from tiddl.utils import (
-    formatTrack,
-    trackExists,
     TidalResource,
     convertFileExtension,
+    formatResource,
+    trackExists,
 )
-from tiddl.metadata import addMetadata, Cover
-from tiddl.exceptions import ApiError, AuthError
-from tiddl.models.constants import TrackArg, ARG_TO_QUALITY
-from tiddl.models.resource import Track, Album
-from tiddl.models.api import PlaylistItems, AlbumItemsCredits
+
+from ..ctx import Context, passContext
 
 SinglesFilter = Literal["none", "only", "include"]
 
@@ -73,28 +71,179 @@ def DownloadCommand(
 ):
     """Download resources"""
 
+    # TODO: pretty print
     logging.debug(
         (QUALITY, TEMPLATE, THREADS_COUNT, DO_NOT_SKIP, SINGLES_FILTER)
     )
 
+    DOWNLOAD_QUALITY = ARG_TO_QUALITY[
+        ctx.obj.config.download.quality or QUALITY
+    ]
+
     api = ctx.obj.getApi()
 
+    progress = Progress(
+        TextColumn("{task.description}"),
+        BarColumn(bar_width=40),
+        console=ctx.obj.console,
+        transient=True,
+        auto_refresh=True,
+    )
+
+    def handleItemDownload(
+        item: Union[Track, Video],
+        path: Path,
+        cover_data=b"",
+        credits: List[AlbumItemsCredits.ItemWithCredits.CreditsEntry] = [],
+    ):
+        if isinstance(item, Track):
+            track_stream = api.getTrackStream(item.id, quality=DOWNLOAD_QUALITY)
+            urls, extension = parseTrackStream(track_stream)
+        elif isinstance(item, Video):
+            video_stream = api.getVideoStream(item.id)
+            urls = parseVideoStream(video_stream)
+            extension = ".ts"
+        else:
+            raise TypeError(
+                f"Invalid item type: expected an instance of Track or Video, "
+                f"received an instance of {type(item).__name__}. "
+            )
+
+        task_id = progress.add_task(
+            description=f"{type(item).__name__}: {item.title}",
+            start=True,
+            visible=True,
+            total=len(urls),
+        )
+
+        with Session() as s:
+            stream_data = b""
+
+            for url in urls:
+                req = s.get(url)
+                stream_data += req.content
+                progress.advance(task_id)
+
+        path = path.with_suffix(extension)
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+        with path.open("wb") as f:
+            f.write(stream_data)
+
+        if isinstance(item, Track):
+            if item.audioQuality == "HI_RES_LOSSLESS":
+                convertFileExtension(
+                    source_file=path,
+                    extension=".flac",
+                    remove_source=True,
+                    is_video=False,
+                    copy_audio=True,  # extract flac from m4a container
+                )
+
+            addMetadata(path, item, cover_data, credits)
+
+        elif isinstance(item, Video):
+            convertFileExtension(
+                source_file=path,
+                extension=".mp4",
+                remove_source=True,
+                is_video=True,
+                copy_audio=True,
+            )
+
+            # TODO: add metadata
+
+        progress.remove_task(task_id)
+        logging.info(f'✔ "{item.title}"')
+
+    pool = ThreadPoolExecutor(max_workers=THREADS_COUNT)
+
+    def submitItem(item: Union[Track, Video], filename: str):
+        path = ctx.obj.config.download.path / f"{filename}.*"
+
+        logging.debug(f'{type(item).__name__} {item.id} path: "{path}"')
+
+        if not DO_NOT_SKIP:  # check if item is already downloaded
+            if isinstance(item, Track):
+                if trackExists(item.audioQuality, DOWNLOAD_QUALITY, path):
+                    return
+            elif isinstance(item, Video):
+                if path.with_suffix(".mp4").exists():
+                    return
+
+        # TODO: cover, credits
+        future = pool.submit(
+            handleItemDownload, item=item, path=path, cover_data=b"", credits=[]
+        )
+        try:
+            future.result()
+        except Exception as e:
+            logging.error(e)
+
     def handleResource(resource: TidalResource) -> None:
-        pass
+        logging.debug(f"Handling Resource {resource}")
 
-    failed_resources: list[TidalResource] = []
+        match resource.type:
+            case "track":
+                track = api.getTrack(resource.id)
+                logging.info(f"★ Track: {track.title}")
 
+                filename = formatResource(
+                    TEMPLATE or ctx.obj.config.template.track, track
+                )
+
+                submitItem(track, filename)
+
+            case "video":
+                video = api.getVideo(resource.id)
+                logging.info(f"★ Video: {video.title}")
+
+                filename = formatResource(
+                    TEMPLATE or ctx.obj.config.template.video, video
+                )
+
+                submitItem(video, filename)
+
+            case "album":
+                album = api.getAlbum(resource.id)
+                logging.info(f"★ Album: {album.title}")
+                pass
+
+            case "artist":
+                artist = api.getArtist(resource.id)
+                logging.info(f"★ Artist: {artist.name}")
+                pass
+
+            case "playlist":
+                playlist = api.getPlaylist(resource.id)
+                logging.info(f"★ Playlist: {playlist.title}")
+                pass
+
+    progress.start()
+
+    # TODO: remove this
+    ctx.obj.resources.extend(
+        [
+            TidalResource(type="track", id="103805726"),
+            TidalResource(type="album", id="103805723"),
+        ]
+    )
+
+    # TODO: make sure every resource is unique
     for resource in ctx.obj.resources:
         try:
             handleResource(resource)
 
-        except ApiError as e:
-            # TODO: handle rate limit
-            logging.error(e)
-            failed_resources.append(resource)
-
         except AuthError as e:
             logging.error(e)
-            return
+            break
 
-    # TODO: do something with `failed_resources`
+        except ApiError as e:
+            logging.error(e)
+
+            # session does not have streaming privileges
+            if e.sub_status == 4006:
+                break
+
+    pool.shutdown(wait=True)
+    progress.stop()
